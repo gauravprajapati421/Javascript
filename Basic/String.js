6217d91145c86a26437c7cc6453bc1d9a195ca1e
Gaurav Prajapati <gauravprajapati2022@gmail.in>
2024-08-30 18:15:52 +0530

    001

---
+++ Basic/String.js
@@ -0,0 +1,17 @@
+//const name = "gaurav"
+
+
+console.log("gaurav");
+
+const gameName = new String("GAURAV")
+
+const newString = gameName.subtracting(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-9,2)
+console.log(anotherString);
+
+const newStringOne = "gauraav"
+console.log(newStringOne);
+console.log(newStringOne.trim);
+
