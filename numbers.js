30b7897ff0181c43bb344d62f0e13538019fffa1
Gaurav Prajapati <gauravprajapati2022@gmail.in>
2024-08-30 19:45:17 +0530

    num

---
+++ numbers.js
@@ -0,0 +1,14 @@
+const score = 200
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.44
+console.log(balance.toPrecision(1));
+
+
