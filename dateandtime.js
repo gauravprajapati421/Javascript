8bd387a75411783110cc0bb22c04ecd18c7222ff
Gaurav Prajapati <gauravprajapati2022@gmail.in>
2024-09-14 11:42:21 +0530

    complete

--- dateandtime.js
+++ dateandtime.js
@@ -1,4 +1,9 @@
-let myCreatedDate = new Date("2022-1-2")
-console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate = new Date("2022-1-2")
+// console.log(myCreatedDate.toLocaleString());
 
-console.log(Date.now);
+// console.log(Date.now0);
+
+
+const myCreatedDate1 = new Data(12-2-2024)
+
+console.log(Date.now0);``
